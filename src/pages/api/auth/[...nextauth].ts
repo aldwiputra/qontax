907a48af4bd26858b1e7a4f8188981f961d6b2eb
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-06-26 15:32:20 +0700

    add prisma and login oauth with github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,26 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import { Adapter } from 'next-auth/adapters';
 import NextAuth from 'next-auth/next';
 import GithubProvider from 'next-auth/providers/github';
 
-export default NextAuth({
+const prisma = new PrismaClient();
+export const authOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-});
+  callbacks: {
+    session: async ({ session, token, user }: { session: any; token: any; user: any }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,25 +13,21 @@ export default function Home() {
 
   return (
     <div className={` min-h-screen flex justify-center items-center ${inter.className} `}>
-      {session.status === 'unauthenticated' && (
-        <button
-          onClick={() => signIn('github')}
-          className='bg-blue-700 px-6 py-2 rounded-md hover:bg-blue-700/90'>
-          Sign In
-        </button>
-      )}
-      {session.status === 'authenticated' && (
-        <button
-          onClick={() => signOut({ redirect: false })}
-          className='bg-red-700 px-6 py-2 rounded-md hover:bg-red-700/90'>
-          Sign Out
-        </button>
-      )}
-      {session.status === 'loading' && (
-        <button disabled={true} className='bg--700 px-6 py-2 rounded-md bg-gray-800'>
-          Loading...
-        </button>
-      )}
+      <div className='text-center'>
+        <h1>Home</h1>
+        {session.status === 'authenticated' && (
+          <button
+            onClick={() => signOut({ callbackUrl: '/login' })}
+            className='bg-red-700 px-6 py-2 rounded-md hover:bg-red-700/90'>
+            Sign Out
+          </button>
+        )}
+        {session.status === 'loading' && (
+          <button disabled={true} className='bg--700 px-6 py-2 rounded-md bg-gray-800'>
+            Loading...
+          </button>
+        )}
+      </div>
     </div>
   );
 }
