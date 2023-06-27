191ace6b1f5dd0d01015644e8960dcf3169bead8
Aldiansyah Dwi P <aldiansyhdp@gmail.com>
2023-06-27 10:29:25 +0700

    create auth button and logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,23 +41,11 @@ export default function Home() {
         <title>Home | Qontax</title>
       </Head>
       <Layout>
-        <div className={` min-h-screen flex justify-center items-center ${inter.className} `}>
+        <section className={`flex justify-center items-center ${inter.className} `}>
           <div className='text-center'>
             <h1>Home</h1>
-            {session.status === 'authenticated' && (
-              <button
-                onClick={() => signOut({ callbackUrl: '/login' })}
-                className='bg-red-700 px-6 py-2 rounded-md hover:bg-red-700/90'>
-                Sign Out
-              </button>
-            )}
-            {session.status === 'loading' && (
-              <button disabled={true} className='bg--700 px-6 py-2 rounded-md bg-gray-800'>
-                Loading...
-              </button>
-            )}
           </div>
-        </div>
+        </section>
       </Layout>
     </>
   );
